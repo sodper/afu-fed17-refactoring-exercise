5f6c513ccd95259be1f0bd9ced3d47ee602481f0
Per Flitig <per.flitig@gmail.com>
2018-03-29 16:05:43 +0200

    Refaktorisering genomförd under lektion

--- src/rental.js
+++ src/rental.js
@@ -7,7 +7,9 @@
 //
 // var result = new Rental().statement(customer)
 
-var Rental = function () { };
+var Rental = function () {
+  this.statementPresenter = new StatementStringPresenter();
+};
 
 Rental.prototype.statement = function (customer) {
 
@@ -19,47 +21,77 @@ Rental.prototype.statement = function (customer) {
     //EXERCISE NOTE: add more movies if you need
   };
 
-  let total = 0;
-  let points = 0;
-  let result = `Rental Record for ${customer.name}\n`;
-  for (let r of customer.rentals) {
-    let m = movies[r.movieID];
-    let amount = 0;
+  let statement = {
+    customerName: customer.name,
+    total: 0,
+    frequentRenterPoints: 0,
+    movies: []
+  };
 
-    // determine amount for each movie
-    switch (m.code) {
-      case 'regular':
-        amount = 2;
-        if (r.days > 2) {
-          amount += (r.days - 2) * 1.5;
-        }
-        break;
-      case 'new':
-        amount = r.days * 3;
-        break;
-      case 'childrens':
-        amount = 1.5;
-        if (r.days > 3) {
-          amount += (r.days - 3) * 1.5;
-        }
-        break;
+  customer.rentals.forEach((rental) => {
+    let movie = movies[rental.movieID];
+    let statementMovie = {
+      title: movie.title,
+      amount: determineAmountForMovie(movie, rental),
     }
 
-    //add frequent renter points
-    points++;
-    // add bonus for a two day new release rental
-    if (m.code === 'new' && r.days > 2) points++;
+    statement.frequentRenterPoints = calculateFrequentRenterPoints();
+    statement.total += statementMovie.amount;
+    statement.movies.push(statementMovie);
+  });
 
-    //print figures for this rental
-    result += `\t${m.title}\t${amount}\n`;
-    total += amount;
-  }
-  // add footer lines
-  result += `Amount owed is ${total}\n`;
-  result += `You earned ${points} frequent renter points\n`;
-
-  return result;
+  return this.statementPresenter.build(statement);
 };
 
+var StatementStringPresenter = function() {};
+
+StatementStringPresenter.prototype.build = function (statement) {
+  let result = `Rental Record for ${statement.customerName}\n`;
+
+  for (let i=0; i < statement.movies.length; i++) {
+    result += `\t${statement.movies[i].title}\t${statement.movies[i].amount}\n`;
+  }
+
+  result += `Amount owed is ${statement.total}\n`;
+  result += `You earned ${statement.frequentRenterPoints} frequent renter points\n`;
+
+  return result;
+}
+
+function calculateFrequentRenterPoints(movie, rental) {
+  let frequentRenterPoints = 1;
+
+  if (isEligibleForNewMovieBonus(movie, rental)) frequentRenterPoints++;
+
+  return frequentRenterPoints;
+}
+
+function isEligibleForNewMovieBonus(movie, rental) {
+  return movie.code === 'new' && rental.days > 2;
+}
+
+function determineAmountForMovie(movie, rental) {
+  let amount = 0;
+
+  switch (movie.code) {
+    case 'regular':
+      amount = 2;
+      if (rental.days > 2) {
+        amount += (rental.days - 2) * 1.5;
+      }
+      break;
+    case 'new':
+      amount = rental.days * 3;
+      break;
+    case 'childrens':
+      amount = 1.5;
+      if (rental.days > 3) {
+        amount += (rental.days - 3) * 1.5;
+      }
+      break;
+  }
+
+  return amount;
+}
 
 exports.rental = Rental;
